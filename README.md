fcd4cb0fc96e5b81eb18abdbf0e83b7f163a95c9
heliobarolo0586 <heliobarolo0586@gmail.com>
2022-09-17 00:38:32 -0300

    Criadd HTTP methods

---
+++ README.md
@@ -0,0 +1,37 @@
+
+## Dica do Diego, trabalhar com tempo em minutos. 
+
+### Back-end 
+
+## Entidades
+
+### Gamer
+
+id
+title
+bannerUrl
+
+CDN (Content Delivery Network) => 
+
+### Anúncio
+
+id 
+gameId
+name
+yearsPlaying
+discord
+weekDays
+hourStart
+hourEnd
+useVoiceChannel
+createdAt
+
+### Casos de Usos - que no final serão rotas ###
+
+* listagem de novo anúncio
+* Criação de novo anúncio 
+* Listagem de anúncios por game
+* Buscar discord pelo ID do anúncio
+
+
+
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,53 @@
+/********************** ANOTAÇÕES **********************************************
+ *   * HTTP methods / API RESTful methods 
+ *   * HTTP Codes(Ele retorna informações da resposta, como por exemplo erros, 
+ *   * sucesso, tipo e etc...)
+ *   *
+ *   - Métodos GET, POST, PUT, PATCH, DELETE, 
+ *   * 
+ *   * HTTP CODES 
+ *   * Códigos iniciados com:
+ *   * 2 - São de sucesso
+ *   * 3 - Sao de redirecionamento
+ *   * 4 - Erros gerados na nosso aplicação, como um código bugado
+ *   * 5 - Erros inesperados
+ *   * https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+ *   * 
+ */
+
+
 import express from 'express'
 
 const app = express()
 
-app.get('/ads', (request, response) => {
+//listagem de novo anúncio
+app.get('/games', (request, response) => {
+  return response.json([])
+});
+
+//No método post eu devo escrever no plural
+//Criação de novo anúncio 
+app.post('/ads', (request, response) => {
+  return response.status(201).json([])
+
+});
+
+//Listagem de anúncios por game
+app.get('/games/:id/ads', (request, response) => {
+  // const gameId = request.params.id;  
   return response.json([
     {id: 1, name: 'Anuncio 1'},
     {id: 2, name: 'Anuncio 2'},
     {id: 3, name: 'Anuncio 3'},
     {id: 4, name: 'Anuncio 4'},
   ])
-})
+});
 
- app.listen(3333)
+app.get('/games/:id/discord', (request, response) => {
+  return response.json([])
+});
+
+app.listen(3333)
 
  /** ----------------------------- TYPESCRIPT-----------------------------------
   *  _Tipagem estática permite a gente definir o formato que esperamos de cada
